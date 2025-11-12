758602f54aa4fc85bd411ca331be5967d17d7ccb
Elisa Gomez <elysa3148@gmail.com>
2025-11-12 12:08:52 +0100

    Ajout d'un tableau vide au début pour éviter le crash

--- components/Home.js
+++ components/Home.js
@@ -90,22 +90,23 @@ function Home() {
   //   },
   // ];
 
-  const movies = moviesData.map((data, i) => {
-    const isLiked = likedMovies.some((movie) => movie === data.title);
-    return (
-      <Movie
-        key={i}
-        updateLikedMovies={updateLikedMovies}
-        isLiked={isLiked}
-        title={data.title}
-        overview={data.overview}
-        poster={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-        voteAverage={data.vote_average}
-        voteCount={data.vote_count}
-      />
-
-    );
-  });
+  const movies = Array.isArray(moviesData)
+    ? moviesData.map((data, i) => {
+        const isLiked = likedMovies.some((movie) => movie === data.title);
+        return (
+          <Movie
+            key={i}
+            updateLikedMovies={updateLikedMovies}
+            isLiked={isLiked}
+            title={data.title}
+            overview={data.overview}
+            poster={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+            voteAverage={data.vote_average}
+            voteCount={data.vote_count}
+          />
+        );
+      })
+    : [];
 
   return (
     <div className={styles.main}>
